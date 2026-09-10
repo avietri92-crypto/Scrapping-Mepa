c433d9c345c4efee1d8d4fefec01d4494d576508
avietri92-crypto <a.vietri92@gmail.com>
2026-09-10 23:41:18 +0200

    Fix indentation for while loop in main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ with sync_playwright() as p:
     page = context.new_page()                       # 3. Page: una scheda dentro il context
 
     page.goto("https://mepa.it/home/garemepa")                # naviga con quella scheda
-     while pulsante_altri.is_visible():
+    while pulsante_altri.is_visible():
         pulsante_altri.click()
         page.wait_for_timeout(1000)
     titoli=page.locator("#simpleList h4").wait_for() 
