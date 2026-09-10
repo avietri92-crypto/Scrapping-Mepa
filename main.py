cc831bfee16cd7a078960c16d45a15c6da89729c
avietri92-crypto <a.vietri92@gmail.com>
2026-09-10 23:26:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ with sync_playwright() as p:
     page = context.new_page()                       # 3. Page: una scheda dentro il context
 
     page.goto("https://mepa.it/home/garemepa")                # naviga con quella scheda
-    titoli=page.locator(".m-t-10 markkeyw")                             # legge il titolo della pagina
+    titoli=page.locator("#simpleList h4")                             # legge il titolo della pagina
     contatore= titoli.count()
     
     for i in range(contatore):
