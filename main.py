4de5c1584ff23368ca318e9b64fc88ebbfbbad9c
avietri92-crypto <a.vietri92@gmail.com>
2026-09-10 23:38:18 +0200

    Update locator for titles in main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ with sync_playwright() as p:
     page = context.new_page()                       # 3. Page: una scheda dentro il context
 
     page.goto("https://mepa.it/home/garemepa")                # naviga con quella scheda
-    titoli=page.locator(".m-t-10").wait_for() 
+    titoli=page.locator("#simpleList h4").wait_for() 
     pulsante_altri = page.locator("text=Mostra altri risultati")  # adatta al testo/selettore reale
 
     while pulsante_altri.is_visible():
