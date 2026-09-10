56ab860d3a5968c274b80da4d62671e377298201
avietri92-crypto <a.vietri92@gmail.com>
2026-09-10 23:29:08 +0200

    Update locator to wait for element before counting

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ with sync_playwright() as p:
     page = context.new_page()                       # 3. Page: una scheda dentro il context
 
     page.goto("https://mepa.it/home/garemepa")                # naviga con quella scheda
-    titoli=page.locator("#simpleList h4")                             # legge il titolo della pagina
+    titoli=page.locator("#simpleList h4").wait_for()                            
     contatore= titoli.count()
     
     for i in range(contatore):
-        print(titoli.nth(i))
+        print(titoli.nth(i).text_content())
         
     browser.close()  
