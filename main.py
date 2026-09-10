37588562a7a198b50f01c8bed3b353ef14683b9a
avietri92-crypto <a.vietri92@gmail.com>
2026-09-10 22:35:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import requests
+proxies = {
+  "https": "scraperapi:45157fd2ab79ca980e39f985c9868c66@proxy-server.scraperapi.com:8001"
+}
+r = requests.get('https://mepa.it/home/garemepa', proxies=proxies, verify=False)
+print(r.text)
 
